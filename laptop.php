9150df37d20d0a197ed8f9b786e1616f7aeeb590
HuryInsana <161497754+HuryInsana@users.noreply.github.com>
2024-03-05 08:22:01 -0800

    laptop.php caro

---
+++ laptop.php
@@ -0,0 +1,25 @@
+<?php
+
+$laptop = [
+    "Numero de modelo" => "Latitude 7390",
+    "Familia de procesador" => "Intel Kaby Lake U y R(i3/i5/i7 de septima y octava generacion).",
+    "Sistema operativo" => 64 bits,
+    "Memoria" => "SDRAM LPDDR3 de 1866 MHz; hasta 8GB (integrada).",
+    "Pantalla" => "FHD de 13.3 pulgadas, 16:9 (1920x1080).",
+    "Opciones de bateria" => "Adaptador de 45/65/90 W, USB de tipo C.",
+    "Conectividad" => "Puertos USB, HDMI, Ethernet, Wi-fi, Bluetooth.",
+    "Peso" => 1.307 kg,
+    "Placa base" => "Intel Kaby Lake.U iHDCP 2.2 Premium PHC",
+    "Disco duro" => "SK hynix SC311 M.2, Suministo de electricdad",
+    "RAM" => "8GB",
+    "Almacenamiento" => "Hasta 512gb",
+    "Tarjeta Grafica" => "Intel UHD",
+    "Bateria" => "5000 miliamperios",
+    "Microfono" => "Microfonos duales de matriz digital",
+    "Trackpad" => "Persona 2",
+    "Camara web" => "Camara web HD con obturador de privacidad",
+    "Compatiblidad con Lapiz" => "Si",
+    "Modo Tableta" => "No, es una laptop tradicional",
+    "Puertos adicionales" => "Ranura para tarjeta SIM, ranura para tarjeta de memoria microSD",
+
+];
